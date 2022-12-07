95d1aa48a2bba2d0e54bef86acc328e7c66e27e2
rakeshkhaarol <rakeshkharol457@gmail.com>
2022-12-07 14:59:09 +0530

    news 1 and 2 done

---
+++ js/script.js
@@ -0,0 +1,73 @@
+//i can write the js code  in extarnal
+
+
+const news2 ={
+    //1. proprty:value
+        w1:'Chief',
+        w2:"Justice",
+        w3:`DY`,
+        w4:'Chandrachud',
+        w5:"on",
+        w6:'Wednesday',
+        w7:"announced",
+        w8:`the`,
+        w9:"launch",
+        w10:`of`,
+        w11:"the",
+        w12:'android',
+        w13:"version",
+        w14:'of',
+        w15:"the",
+        w16:`mobile`,
+        w17:'app',
+        w18:"for",
+        w19:`the`,
+        w20:'Supreme',
+        w21:"Court",
+        w22:'with',
+        w23:'additional',
+        w24:"features",
+        w25:`with`,
+        w26:'an',
+        w27:"aim",
+        w28:`to`,
+        w29:"enable",
+        w30:'all',
+        w31:`the`,
+        w32:"government",
+        w33:'departments',
+        w34:"to",
+        w35:"see",
+        w36:'their',
+        w37:`pending`,
+        w38:'cases.'
+
+    //2.
+    /*
+     Chief Justice DY Chandrachud on Wednesday announced the launch of the android version 
+     of the mobile app for the Supreme Court with additional features with an aim to enable 
+     all the government departments to see their pending cases.
+    */
+
+    
+}
+
+let p = 'w3'
+let p1 ="w4"
+let p11 ="w11"
+let p12 ="w12"
+let p13 ="w13"
+let p14 ="w14"
+let p15 ="w15"
+let p16 ="w16"
+let p17 ="w17"
+let p18 ="w18"
+let p19 ="w19"
+
+//objectName.propertyName
+            
+//objectName["propertyName"]
+
+//objectName[expression];
+
+window.alert(news2.w1+' '+news2["w2"]+" "+news2[p]+` `+news2[p1]+' '+news2['w5']+' '+news2['w6']+' '+news2[`w7`]+' '+news2.w8+' '+news2.w9+' '+news2.w10+" "+news2[p11]+" "+news2[p12]+" "+news2[p13]+" "+news2[p14]+" "+news2[p15]+" "+news2[p16]+" "+news2[p17]+" "+news2[p18]+" "+news2[p19])
